0014658ac919c8e5b5fdc34440378794a44b19f3
Noble-Chain <sawchain17@gmail.com>
2022-07-06 20:59:01 +0630

    register ready

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row vh-100">
+        <!--Dashboard -->
+        <div class="col-lg-2 bg-danger">
+            <div class="">
+                <small>Dashboard ></small>
+                <p></p>
+            </div>
+        </div>
+        <div class="col-lg-10 bg-secondary"></div>
+    </div>
+</div>
+@endsection
