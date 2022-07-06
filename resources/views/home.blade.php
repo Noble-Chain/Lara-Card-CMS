66c184cd52c94c8ea52b0cf542dce2f721960295
Noble-Chain <sawchain17@gmail.com>
2022-07-06 21:58:41 +0630

    ui starter ready

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,56 @@
 @section('content')
 <div class="container-fluid">
     <div class="row vh-100">
-        <!--Dashboard -->
-        <div class="col-lg-2 bg-danger">
-            <div class="">
-                <small>Dashboard ></small>
-                <p></p>
+        <!--Sidebar start-->
+        @include('layouts.sidebar')
+        <!--sidebar end -->
+        <!--content start -->
+        <div class="col-lg-10 content-bg">
+            <div class="p-5">
+
+                <div class="d-flex justify-content-between align-items-center g-1">
+                    <div class="col-6">
+                        <h3>Cards | 18 <small>PCS</small></h3>
+                    </div>
+                    <div class="col-4 d-flex align-items-center justify-content-between w-25 ">
+                        <small>Select All Cards</small>
+                        <input type="checkbox">
+                        <button class="btn btn-outline-secondary text-uppercase rounded-pill px-4">Export to PDF</button>
+                    </div>
+                    <div class="mx-3 break">  </div>
+                    <div class="col-2">
+                        <a href="#" class="btn btn-success rounded-pill px-4">
+                            <i class="bi bi-plus"></i>
+                            Create Card
+                        </a>
+                    </div>
+                </div>
+
+                <ul class="nav mt-3 border-bottom">
+                    <li class="nav-item me-3">
+                      <a class="tab-spacing text-secondary ps-0 nav-link fs-6 text-uppercase active" aria-current="page" href="#">All cards</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="tab-spacing text-secondary ps-0 nav-link fs-6 text-uppercase" href="#">
+                           <div class="d-flex align-items-center ">
+                                <div class="active-dot me-2"></div>
+                                Active
+                           </div>
+                        </a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="tab-spacing text-secondary ps-0 nav-link fs-6 text-uppercase" href="#">
+                           <div class="d-flex align-items-center ">
+                                <div class="inactive-dot me-2"></div>
+                                Inactive
+                           </div>
+                        </a>
+                    </li>
+
+                  </ul>
+
             </div>
         </div>
-        <div class="col-lg-10 bg-secondary"></div>
     </div>
 </div>
 @endsection
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,34 @@
+<div class="col-lg-2 sidebar-bg">
+    <div class="p-5 text-light">
+        <p class="text-white-50 mb-2 small text-uppercase side-bar-menu">Dashboard ></p>
+
+        <div class="">
+            <a class="d-flex align-items-center text-decoration-none text-white-50 sidebar-active" href="#">
+                <i class="bi bi-card-text m-2"></i>
+                <p class="mb-0 l side-bar-menu">Cards</p>
+            </a>
+        </div>
+        <div class="">
+            <a class="d-flex align-items-center text-decoration-none text-white-50" href="#">
+                <i class="bi bi-card-text m-2"></i>
+                <p class="mb-0 l side-bar-menu">Analytics</p>
+            </a>
+        </div>
+        <div class="mb-4">
+            <a class="d-flex align-items-center text-decoration-none text-white-50" href="#">
+                <i class="bi bi-card-text m-2"></i>
+                <p class="mb-0 l side-bar-menu">Report</p>
+            </a>
+        </div>
+
+        <p class="text-white-50 mb-2 small text-uppercase side-bar-menu">Setting</p>
+
+        <div class="">
+            <a class="d-flex align-items-center text-decoration-none text-white-50" href="#">
+                <i class="bi bi-card-text m-2"></i>
+                <p class="mb-0 l side-bar-menu">Users</p>
+            </a>
+        </div>
+
+    </div>
+</div>
